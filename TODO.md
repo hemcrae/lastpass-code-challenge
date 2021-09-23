2d1b16225f4087f0e6d36010c4f7ea6fd10a10e4
hemcrae <halley.mcrae@gmail.com>
2021-09-23 14:21:26 -0400

    logic to return 401 if given username/password does not match, complete

--- TODO.md
+++ TODO.md
@@ -2,5 +2,5 @@
 
 - [ ] Backend
   - [x] Initialize backend with express
-  - [ ] Make /login endpoint that returns status 200 (no content)
-  - [ ] Create logic to return 401 if given username/password is not matching mock username/password
+  - [x] Make /login endpoint that returns status 200 (no content)
+  - [x] Create logic to return 401 if given username/password is not matching mock username/password
--- server/server.js
+++ server/server.js
@@ -2,12 +2,34 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+app.use(express.json());
+
+const users = [
+  {
+    username: "username",
+    password: "password",
+  },
+];
+
 app.get("/", (req, res) => {
   res.send("Hello");
 });
 
 app.get("/login", (req, res) => {
   res.status(200).send();
+});
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find((u) => {
+    return u.username === username && u.password === password;
+  });
+
+  if (user) {
+    res.status(200).send();
+  } else {
+    res.status(401).send();
+  }
 });
 
 app.listen(PORT, () => {
